3eaf56621d42e5160338b9f4505669d7984e3b4b
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2014-02-23 15:34:44 -0500

    Add EagerWorker.

---
+++ include/eager_worker.hh
@@ -0,0 +1,42 @@
+// Author: Jose M. Faleiro (faleiro.jose.manuel@gmail.com)
+// 
+
+#ifndef 		EAGER_WORKER_HH_
+#define 		EAGER_WORKER_HH_
+
+#include <lock_manager.hh>
+#include <concurrent_queue.h>
+#include <pthread.h>
+#include <util.h>
+
+class EagerWorker {
+private:
+    LockManager			*m_lock_mgr;			// Global lock manager
+    SimpleQueue 		*m_txn_input_queue;		// Thread-local input queue
+    SimpleQueue 		*m_output_queue;		// Thread-local output queue
+    int 				m_cpu_number;			// CPU to which to bind
+    volatile uint64_t	m_start_signal;			// Flag indicating we've begun
+    pthread_t 			m_worker_thread;		// Worker thread
+
+    // The worker threads starts executing in this function
+    static void*
+    BootstrapWorker(void *arg);
+
+    // Worker thread function
+    virtual void
+    WorkerFunction();
+
+public:
+    EagerWorker(LockManager *mgr, SimpleQueue *input_queue, 
+                SimpleQueue *output_queue, int cpu);
+    
+    // Should be called from the co-ordinating function
+    //
+    // Spawns a thread dedicated to processing txns pinned to a particular cpu 
+    // (as specified by m_cpu_number).
+    virtual void
+    Run();    
+
+};
+
+#endif 		 // EAGER_WORKER_HH_
---
+++ src/eager_worker.cc
@@ -0,0 +1,44 @@
+// Author: Jose M. Faleiro (faleiro.jose.manuel@gmail.com)
+//
+
+#include <eager_worker.hh>
+
+EagerWorker::EagerWorker(LockManager *mgr, SimpleQueue *input_queue, 
+                         SimpleQueue *output_queue, int cpu) {
+    m_lock_mgr = mgr;
+    m_txn_input_queue = input_queue;
+    m_output_queue = output_queue;
+    m_cpu_number = cpu;
+    m_start_signal = 0;
+}
+
+void
+EagerWorker::Run() {
+    assert(m_start_signal == 0);    
+    
+    // Kickstart the worker thread
+    pthread_create(&m_worker_thread, NULL, BootstrapWorker, this);
+
+    // Wait for the newly spawned thread to report that it has successfully 
+    // initialized
+    while (!m_start_signal)
+        ;
+}
+
+void*
+EagerWorker::BootstrapWorker(void *arg) {
+    EagerWorker *worker = (EagerWorker*)arg;
+    fetch_and_increment(&worker->m_start_signal);
+    worker->WorkerFunction();
+}
+
+void
+EagerWorker::WorkerFunction() {
+    while (true) {
+        Action *txn = (Action*)m_txn_input_queue->DequeueBlocking();
+        m_lock_mgr->Lock(txn);
+        txn->LaterPhase();
+        m_lock_mgr->Unlock(txn);
+        m_output_queue->EnqueueBlocking((uint64_t)txn);
+    }
+}
