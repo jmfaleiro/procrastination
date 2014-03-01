8266fab5c1dd9330d3e6293bd012f8c3a1bf5bb9
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2014-03-01 12:28:11 -0500

    Forgot to add new Runnable class for abstracting away cpu binding details from individual classes such as lazy scheduler/worker etc.

---
+++ include/runnable.hh
@@ -0,0 +1,30 @@
+#ifndef 		RUNNABLE_HH_
+#define 		RUNNABLE_HH_
+
+#include <pthread.h>
+#include <cpuinfo.h>
+#include <util.h>
+#include <cassert>
+#include <iostream>
+
+class Runnable {
+private:
+    volatile uint64_t 		m_start_signal;
+    int 					m_cpu_number;
+    pthread_t 				m_thread;
+
+protected:
+    virtual void
+    StartWorking() = 0;
+
+    static void*
+    Bootstrap(void *arg);
+
+public:    
+    Runnable(int cpu_number);
+    
+    void
+    Run();
+};
+
+#endif		//  RUNNABLE_HH_
